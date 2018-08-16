537fb40b544e9b793dd18c6f411426d2382efa73
Adam Schachne <adamschachne@live.com>
2018-08-15 22:07:35 -0700

    unitedspaceventures is a non image cmd

--- src/Network/utils/buildGraph.js
+++ src/Network/utils/buildGraph.js
@@ -3,7 +3,8 @@ import { connect } from "net";
 
 const NON_IMAGE_COMMANDS = {
   "UNKNOWN COMMAND": true,
-  "?welcomehome": true
+  "?welcomehome": true,
+  "?unitedspaceventures": true
 };
 
 export default ({ items, updated }) => {
